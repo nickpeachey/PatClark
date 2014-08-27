e6ad5abe803b7a6d14feee2d4a04317aab45c0d4
Nick Peachey <nick.peachey@gmail.com>
2014-08-27 12:24:21 +0100

    try it now

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Patclarkcounselling
+module Paxtclarkcounselling
   class Application < Rails::Application
 
     config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
